82a1fbe6b4526427cd8924013aa71a332bce9ded
Samuel Cha <sunchipssam@gmail.com>
2018-03-26 15:10:36 -0700

    Implemented read from fs
    Right now, only first program is loaded into memory.

--- project/ls8/ls8.js
+++ project/ls8/ls8.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 const RAM = require('./ram');
 const CPU = require('./cpu');
 
@@ -6,30 +8,30 @@ const CPU = require('./cpu');
  *
  * TODO: load this from a file on disk instead of having it hardcoded
  */
-function loadMemory() {
+function loadMemory(program) {
   // Hardcoded program to print the number 8 on the console
 
-  const program = [
-    // print8.ls8
-    // "10011001", // LDI R0,8  Store 8 into R0
-    // "00000000",
-    // "00001000",
-    // "01000011", // PRN R0    Print the value in R0
-    // "00000000",
-    // "00000001"  // HLT       Halt and quit
-    '10011001', // LDI R0,8
-    '00000000',
-    '00001000',
-    '10011001', // LDI R1,9
-    '00000001',
-    '00001001',
-    '10101010', // MUL R0,R1 <---
-    '00000000',
-    '00000001',
-    '01000011', // PRN R0
-    '00000000',
-    '00000001', // HLT
-  ];
+  //   const program = [
+  // print8.ls8
+  // "10011001", // LDI R0,8  Store 8 into R0
+  // "00000000",
+  // "00001000",
+  // "01000011", // PRN R0    Print the value in R0
+  // "00000000",
+  // "00000001"  // HLT       Halt and quit
+  //     '10011001', // LDI R0,8
+  //     '00000000',
+  //     '00001000',
+  //     '10011001', // LDI R1,9
+  //     '00000001',
+  //     '00001001',
+  //     '10101010', // MUL R0,R1 <---
+  //     '00000000',
+  //     '00000001',
+  //     '01000011', // PRN R0
+  //     '00000000',
+  //     '00000001', // HLT
+  //   ];
 
   // Load the program into the CPU's memory a byte at a time
   for (let i = 0; i < program.length; i++) {
@@ -44,7 +46,20 @@ function loadMemory() {
 let ram = new RAM(256);
 let cpu = new CPU(ram);
 
-// TODO: get name of ls8 file to load from command line
+const sterilize = program => {
+  return program
+    .filter(line => line !== '' && line[0] !== '#')
+    .map(line => line.slice(0, 8));
+};
+
+const programs = process.argv
+  .slice(2, process.argv.length)
+  .map(program => sterilize(fs.readFileSync(program, 'utf8').split('\n')));
+
+// programs.forEach(program => loadMemory(program));
+
+/* for now just load first program */
+loadMemory(programs[0]);
 
 loadMemory(cpu);
 
